8bf9bb3c0027c9759ec6556784db5abc989b7d9b
yashkurkure <yash.kurkure1903@gmail.com>
2021-05-20 16:02:10 +0530

    Comments/Notes added to ExampleService src files

--- ExampleService/app/src/main/aidl/com/cs478/services/IExampleAIDL.aidl
+++ ExampleService/app/src/main/aidl/com/cs478/services/IExampleAIDL.aidl
@@ -6,7 +6,19 @@ import com.cs478.services.ExampleObject;
 
 //package com.cs478.exampleservice.ExampleObject;
 
-// Declare any non-default types here with import statements
+/**
+*      * Notes: AIDL files define an interface which acts as an API for the clients that
+       *          bind to this service. The studio takes the aidl file with and auto
+       *          generates an abstract class that implements the interface.
+       *
+       *          The auto generated abstract class can be found in the java(generated) folder.
+       *          This class will be named as "Default". In respect with this application you can
+       *          find this class in IExampleAIDL.java inside the java(generated) folder in the
+       *          appropriate package.
+       *
+       *          Important: If you make a change to the aidl file, you must clean and rebuild the
+       *          project. After doing this changes will be reflected in the generated abstract class.
+* */
 
 interface IExampleAIDL {
 
--- ExampleService/app/src/main/java/com/cs478/exampleservice/MyService.java
+++ ExampleService/app/src/main/java/com/cs478/exampleservice/MyService.java
@@ -31,6 +31,10 @@ public class MyService extends Service {
     //Initialized and 5 Example Objects loaded in OnCreate()
     ArrayList<ExampleObject> list;
 
+    /**
+     * onCreate() called when the service is created
+     * in this case whenever the first client binds to it.
+     * */
     @Override
     public void onCreate() {
         super.onCreate();
@@ -62,6 +66,34 @@ public class MyService extends Service {
     }
 
 
+    /**
+     *
+     * Notes: AIDL files define an interface which acts as an API for the clients that
+     *          bind to this service. The studio takes the aidl file with an java interface and auto
+     *          generates an abstract class that implements the interface.
+     *
+     *          The auto generated abstract class can be found in the java(generated) folder.
+     *          This class will be named as "Default". With respect to this application you can
+     *          find this class in IExampleAIDL inside the java(generated) folder in the
+     *          appropriate package.
+     *
+     * Important: >If you make a change to the aidl file, you must clean and rebuild the
+     *              project. After doing this, changes will be reflected in the generated abstract class.
+     *              This would mean that you would have to update the stub accordingly to the new API.
+     *
+     *             >If this client deals with multiple clients, use synchronized block in the implementation
+     *             of the methods.
+     *
+     *
+     *
+     *
+     * >IExampleAIDL is the interface defined in the aidl files.
+     *
+     * >This stub uses implements the methods defined in the
+     *  generated abstract class which implements the IMusicCentral interface.
+     *
+     * >mBinder is the IBinder object that is returned to the client in the onBind() method.
+     * */
     private final IExampleAIDL.Stub mBinder = new IExampleAIDL.Stub() {
         @Override
         public ExampleObject getExampleObjectAt(int i) throws RemoteException {
@@ -103,7 +135,30 @@ public class MyService extends Service {
         }
     };
 
-
+    /**
+     *
+     * Notes: For a class that extends service this method is compulsory to implement.
+     *          This method returns the Binder object to the client, which the client
+     *          can use to make calls to the service. The API for the calls is defined in
+     *          the aidl files using an interface. For more on aidl files read the Notes for the
+     *          Stub.
+     *
+     *          Keep in mind that no matter how many clients bind to the service, THIS service
+     *          would always return the SAME IBinder object. Hence, to prevent race conditions
+     *          when multiple clients bind, it is important to use synchronized blocks inside the
+     *          implementation of the API methods (found in the stub).
+     *
+     *          If you want each client to be handled on a separate thread, you would have to use
+     *          either use JobIntentService or define your own threading construct for the service.
+     *
+     *          Since, this service does not have a threading construct it will return the same binder
+     *          object to all clients.
+     *
+     *
+     *
+     * onBind() called when a new client binds to the service.
+     * It returns the IBinder object to the
+     * */
     @Nullable
     @Override
     public IBinder onBind(Intent intent) {
@@ -123,6 +178,17 @@ public class MyService extends Service {
     private Notification notification;
     private static String CHANNEL_ID = "Example channel id" ;
 
+
+    /**
+     * createNotificationChannel()
+     * Creates a notification channel for the notification of the foreground service.
+     *
+     * Notes: All foreground services need to be accompanied with a notification whenever they are running.
+     *          This was made as a requirement for running a foreground service.
+     *
+     *
+     *
+     * */
     private void createNotificationChannel()
     {
         CharSequence name = "Example Service Notification";
